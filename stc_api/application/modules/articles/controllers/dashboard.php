56df396b9a659a0a79eb7c8e5fcb32df7618f0a3
sunil15 <sunilnalawade15@gmail.com>
2014-08-28 18:11:40 +0530

    Resize Image when uploaded in gallery item
    Upload image to main server if any external image url's.
    Resize uploaded images to width 700px and appropriate height if it's
    width is grater than 700px.

--- stc_api/application/modules/articles/controllers/dashboard.php
+++ stc_api/application/modules/articles/controllers/dashboard.php
@@ -56,75 +56,195 @@ class Dashboard extends MX_Controller{
         public function addMultipleItems($tid=0,$bid=0,$page="ALL"){
             $msg="";
             if($this->input->post())
-		{
-			$gitems=$this->input->post('galleryItems');// exit;
-                        
-			$galleryItemIds=explode(',',$gitems);
-                        
-			//echo "<pre>"; print_r($galleryItemIds); echo "</pre>";
-			foreach($galleryItemIds as $item){
-                            $articleData=array();
-				// Check blog id present #start.
-				if($this->input->post('blogID')) :
-					// Check articleImage present #start.
-                            $slug = url_title($this->input->post('articleTitle_'.$item), 'dash', TRUE);
-							$articleData['articleTitle']=$this->input->post('articleTitle_'.$item);
-							$articleData['slug']=$slug;
-							$articleDescription = $this->input->post('articleDescription_'.$item);
-							if(strpos($articleDescription,"’"))
-							{                              
-								$articleDescription = str_replace("’","'",$articleDescription);
-							}
-							if(strpos($articleDescription,"‘"))
-							{                              
-								$articleDescription = str_replace("‘","'",$articleDescription);
-							}
-							if(strpos($articleDescription,'“'))
-							{                              
-								$articleDescription = str_replace('“','"',$articleDescription);
-							}
-							if(strpos($articleDescription,'”'))
-							{                              
-								$articleDescription = str_replace('”','"',$articleDescription);
-							}
-							$articleData["articleDescription"]=$articleDescription;
-//$articleData['articleDescription']=$this->input->post('articleDescription_'.$item);
-							$articleData['blogID']=$this->input->post('blogID');
-							$articleData['createdBy']=$this->session->userData('userID');
-							$articleData['createdDate']=date('Y-m-d');
-							
-	 //echo "<pre>"; print_r($articleData); echo "</pre>"; exit;
-							 $this->load->model('article');
-							 $insert_id=$this->article->add($articleData);
-							 if($insert_id){
-								if($this->input->post('templateID')){
-									$this->load->model("template");
-									$tid=$this->input->post('templateID');
-									$templateData=array(
-										"htmlCreated"=>"Update",
-										"updatedBy"=>$this->session->userData('userID'),
-										"updatedDate"=>date('Y-m-d'),
-									);	
-									$this->template->update($templateData,$tid);
-								}
-								$msg.="Gallery Item $item : ".$this->input->post('articleTitle_'.$item)." added successfully.<br/>";
-							}
-							else
-							{
-								 $msg.="Gallery Item $item : ".$this->input->post('articleTitle_'.$item)." Not added. Please try again.<br/>";
-							}
-					
-				else :
-					$msg.="Gallery Item $item : ".$this->input->post('articleTitle_'.$item)." Not added. Please select Post.<br/>";
-				endif;
-				// Check blog id present #end.
+            {
+		$gitems=$this->input->post('galleryItems');// exit;
+                $galleryItemIds=explode(',',$gitems);
+                //echo "<pre>"; print_r($galleryItemIds); echo "</pre>";
+		foreach($galleryItemIds as $item){
+                    $articleData=array();
+                    // Check blog id present #start.
+                    if($this->input->post('blogID')) :
+                        // Check articleImage present #start.
+                        $slug = url_title($this->input->post('articleTitle_'.$item), 'dash', TRUE);
+			$articleData['articleTitle']=$this->input->post('articleTitle_'.$item);
+			$articleData['slug']=$slug;
+			$articleDescription = $this->input->post('articleDescription_'.$item);
+			if(strpos($articleDescription,"’"))
+			{                              
+                            $articleDescription = str_replace("’","'",$articleDescription);
 			}
-                         //echo "br:".$item; echo "<pre>"; print_r($_POST); echo "</pre>"; exit;
-			echo $msg;
-                        
+			if(strpos($articleDescription,"‘"))
+			{                              
+                            $articleDescription = str_replace("‘","'",$articleDescription);
+			}
+			if(strpos($articleDescription,'“'))
+			{                              
+                            $articleDescription = str_replace('“','"',$articleDescription);
+			}
+			if(strpos($articleDescription,'”'))
+			{                              
+                            $articleDescription = str_replace('”','"',$articleDescription);
+			}
+                        $description="";
+                        $description = $this->str_get_html($articleDescription);
+                        $articleData["articleDescription"]=$description;
+                        error_reporting(E_ALL);
+                        //$articleData['articleDescription']=$this->input->post('articleDescription_'.$item);
+			$articleData['blogID']=$this->input->post('blogID');
+			$articleData['createdBy']=$this->session->userData('userID');
+			$articleData['createdDate']=date('Y-m-d');
+			//echo "<pre>"; print_r($articleData); echo "</pre>"; exit;
+			$this->load->model('article');
+			$insert_id=$this->article->add($articleData);
+			if($insert_id){
+                            if($this->input->post('templateID')){
+				$this->load->model("template");
+				$tid=$this->input->post('templateID');
+				$templateData=array(
+                                    "htmlCreated"=>"Update",
+                                    "updatedBy"=>$this->session->userData('userID'),
+                                    "updatedDate"=>date('Y-m-d'),
+				);	
+				$this->template->update($templateData,$tid);
+                            }
+                            $msg.="Gallery Item $item : ".$this->input->post('articleTitle_'.$item)." added successfully.<br/>";
+			}
+			else
+			{
+                            $msg.="Gallery Item $item : ".$this->input->post('articleTitle_'.$item)." Not added. Please try again.<br/>";
+			}
+                    else :
+			$msg.="Gallery Item $item : ".$this->input->post('articleTitle_'.$item)." Not added. Please select Post.<br/>";
+                    endif;
+                    // Check blog id present #end.
 		}
+                //echo "br:".$item; echo "<pre>"; print_r($_POST); echo "</pre>"; exit;
+		echo $msg;
+            }
         }
-        
+         // get html dom from string
+        function str_get_html($html)
+        {
+            error_reporting(0);
+            $this->load->library('simpleimage');
+            $doc = DOMDocument::loadHTML($html);
+            $c =0;
+            foreach($doc->getElementsByTagName('img') as $image){
+               if($image->hasAttribute('src')){
+                    $url="";
+                    //$str_contains="192.168.1.80/addmed/stc_api";
+                    //$str_contains="api.socialtrafficcenter.com";
+                     //$str_contains="demo.socialtrafficcenter.com/api_demo";
+                    
+                    $url = $image->getAttribute('src');
+                    if (strpos($url,$str_contains) === false) {
+                                              
+                        if($url){
+                        $file = fopen($url,"rb");
+                        if($file){
+                        $directory = "./uploads/article_images/"; // Directory to upload files to.
+                        $valid_exts = array("jpg","jpeg","gif","png"); // default image only extensions
+                        $ext = end(explode(".",strtolower(basename($url))));
+                        if(in_array($ext,$valid_exts)){
+                        $rand = rand(1000,9999);
+                        $filename = $rand . basename($url);
+                        $newfile = fopen($directory . $filename, "wb"); // creating new file on local server
+                        if($newfile){
+                        while(!feof($file)){
+                        // Write the url file to the directory.
+                        fwrite($newfile,fread($file,1024 * 8),1024 * 8); // write the file to the new directory at a rate of 8kb/sec. until we reach the end.
+                        }
+                        
+                         $uploaded_file_url=  base_url()."uploads/article_images/$filename";
+                         $image->setAttribute('src','');
+                         $image->setAttribute('src',$uploaded_file_url);
+                               
+                        
+                        } else { echo 'Could not establish new file ('.$directory.$filename.') on local server. Be sure to CHMOD your directory to 777.'; }
+                        } else { echo 'Invalid file type. Please try another file.'; }
+                        } else { echo 'Could not locate the file: '.$url.''; }
+                        
+                      }                       
+                        
+                    }
+                    $temp_url="";
+                    $temp_url=$image->getAttribute('src');
+                    $size=getimagesize($temp_url);
+                    $width=$size[0];
+                    if($width>700){
+                        $rand = rand(1000,9999);
+                        $filename = basename($temp_url);
+                        $target_filename = $rand.'_'.basename($temp_url);
+                        $file_location = 'uploads/article_images/'; # Image folder Path
+                        $this->simpleimage->load($file_location.$filename);
+                        $this->simpleimage->resizeToWidth(700);
+                        $this->simpleimage->save($file_location.$target_filename);
+                        $uploaded_url=$file_location.$target_filename;
+                        $imageUrl=base_url(). $uploaded_url;
+                        $image->setAttribute('src','');
+                        $image->setAttribute('src',$imageUrl);
+                    }
+                 }
+                $c = $c+1;
+            }
+            // save html
+            $html=$doc->saveHTML();
+            return $html;
+        }
+        /**
+        * Image resize
+        * @param int $width
+        * @param int $height
+        */
+       function resize($width, $height,$filename){
+         /* Get original image x y*/
+
+         $imageinfo=getimagesize($filename);
+         $w=$imageinfo['0'];
+         $h=$imageinfo['1'];
+         $type=$imageinfo['mime'];
+         //list($w, $h, $type_num, $widthHeight, $bits, $type ) = getimagesize($filename);
+         //print_r($h);  exit;
+         /* calculate new image size with ratio */
+         $ratio = max($width/$w, $height/$h);
+         $h = ceil($height / $ratio);
+         $x = ($w - $width / $ratio) / 2;
+         $w = ceil($width / $ratio);
+         $rand = rand(1000,9999);
+         $new_filename = $rand.'_'.basename($filename);
+         /* new file name */
+         $path = 'uploads/article_images/'.$new_filename;
+         /* read binary data from image file */
+         $imgString = file_get_contents($filename);
+         /* create image from string */
+         $image = imagecreatefromstring($imgString);
+         $tmp = imagecreatetruecolor($width, $height);
+         imagecopyresampled($tmp, $image,
+           0, 0,
+           $x, 0,
+           $width, $height,
+           $w, $h);
+         /* Save image */
+         switch ($type) {
+           case 'image/jpeg':
+             imagejpeg($tmp, $path, 100);
+             break;
+           case 'image/png':
+             imagepng($tmp, $path, 0);
+             break;
+           case 'image/gif':
+             imagegif($tmp, $path);
+             break;
+           default:
+             exit;
+             break;
+         }
+         return $path;
+         /* cleanup memory */
+         imagedestroy($image);
+         imagedestroy($tmp);
+       }
+       
 	public function addmultiple($tid=0,$bid=0,$page="ALL"){
 		$data[]="";
 		$msg="";
@@ -234,7 +354,11 @@ class Dashboard extends MX_Controller{
 					{                              
 						$articleDescription = str_replace('”','"',$articleDescription);
 					}
-					$articleData["articleDescription"]=$articleDescription;
+                                        $description="";
+                                        $description = $this->str_get_html($articleDescription);
+                                        $articleData["articleDescription"]=$description;
+                                        error_reporting(E_ALL);
+					//$articleData["articleDescription"]=$articleDescription;
 					$articleData["blogID"]=$this->input->post('blogID');
 					$articleData["updatedBy"]=$this->session->userData('userID');
 					$articleData["updatedDate"]=date('Y-m-d');
